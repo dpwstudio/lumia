6ea5f2fd1b9a90ce0c175209b7c710deb6afed56
dpwstudio <dpw.contact@gmail.com>
2021-03-29 22:57:25 +0200

    css

--- src/app/modules/layout/components/header/header.component.ts
+++ src/app/modules/layout/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   showDevis = false;
+  showContact = false;
   constructor() { }
 
   ngOnInit(): void {
